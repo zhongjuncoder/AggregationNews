912a8ff26532d28f8d523866bbfbcdd1990c0ab9
zhongjuncoder <1247660633@qq.com>
2017-09-24 23:25:53 +0800

    add refreshLayout to webview

--- app/src/main/java/com/juncoder/aggregationnews/activity/NewsDetailActivity.java
+++ app/src/main/java/com/juncoder/aggregationnews/activity/NewsDetailActivity.java
@@ -1,9 +1,14 @@
 package com.juncoder.aggregationnews.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,19 +23,58 @@ import com.juncoder.aggregationnews.R;
 public class NewsDetailActivity extends AppCompatActivity {
 
     private WebView mWebView;
-
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_detail_layout);
-        String url = getIntent().getStringExtra("url");
+        final String url = getIntent().getStringExtra("url");
 
+        final SwipeRefreshLayout refreshLayout = (SwipeRefreshLayout) findViewById(R.id.news_refresh);
+        refreshLayout.setProgressBackgroundColorSchemeResource(R.color.white);
+        refreshLayout.setColorSchemeResources(R.color.colorPrimary);
         mWebView = (WebView) findViewById(R.id.news_webview);
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress > 50) {
+                    refreshLayout.setRefreshing(false);
+                }
+            }
+        });
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                refreshLayout.setRefreshing(true);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                refreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                refreshLayout.setRefreshing(false);
+            }
+
+        });
         mWebView.loadUrl(url);
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mWebView.loadUrl(url);
+            }
+        });
     }
 
     @Override
